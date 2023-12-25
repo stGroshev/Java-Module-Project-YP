750a66639406df57f3123a35d00dd10a1ae6b6da
Stepan Groshev <st.groshev96@gmail.com>
2023-12-25 17:43:38 +0300

    Выполнил первый пункт практического задания, консольное приложение запрашивает количество персон для разделения счета, но пока еще не может отработать ввод числа буквами. Также создал класс Product, с помощью которого буду создавать объекты данного класса, для создания списка заказанных позиций из общего счета.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,33 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int numberOfPerson;
+        System.out.println("На скольких человек необходимо разделить счёт...");
+
+        try {
+            numberOfPerson = scanner.nextInt();
+            checkingUserInput(numberOfPerson);
+        } catch (InputMismatchException e) {
+            System.out.println("Введите корректное значение используя цифры, а не буквы...");
+        }
+
+
+    }
+    static void checkingUserInput (int number){
+        while (true) {
+            if (number == 1) {
+                System.out.println("Введите значение больше 1-ого...");
+                number = scanner.nextInt();
+            } else if (number > 1) {
+                System.out.println("Значение корректно...");
+                break;
+            } else {
+                System.out.println("Введите корректное значение, целое число больше 1-ого...");
+                number = scanner.nextInt();
+            }
+        }
     }
 }
---
+++ src/main/java/Product.java
@@ -0,0 +1,9 @@
+public class Product {
+    String nameProduct = "name";
+    Double costProduct;
+
+    Product(String nameProduct, Double costProduct){
+        nameProduct = this.nameProduct;
+        costProduct = this.costProduct;
+    }
+}
