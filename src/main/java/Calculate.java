1bdec048c15e8b7eefb8cf6a93c1b3e9b76a9a38
Stepan Groshev <st.groshev96@gmail.com>
2023-12-27 18:03:06 +0300

    Реализовал 2-ой и 3-ий пункты практической работы, осталось сделать отформатированный вывод стоимости позиций из счета и суммы распила, а также добавить обозначение валюты.

--- src/main/java/Calculate.java
+++ src/main/java/Calculate.java
@@ -1,13 +1,23 @@
-public class Calculate {
-    Double sum;
-    int numberOfPerson;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
-    Calculate (Double sum, int numberOfPerson){
-        this.sum = sum;
-        this.numberOfPerson = numberOfPerson;
+public class Calculate {
+    Double sum = 0.0;
+    ArrayList<Product> listProduct = new ArrayList<Product>();
+    void sum (Double costProduct){
+        sum = sum + costProduct;
+    }
+    void addingProductsToCalculate(Product product){
+        listProduct.add(product);
     }
     double division(Double sum, int numberOfPerson){
         return sum / numberOfPerson;
+    }
+
+    void addingProductsToConsole(){
+        for (int i = 0; i < listProduct.size(); i++ ){
+            System.out.println((i + 1) +"-я позиция по счету: " + listProduct.get(i).nameProduct + " стоимость которой = " + listProduct.get(i).costProduct);
+        }
     }
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,37 +6,36 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int numberOfPerson = 0;
+        int numberOfPerson;
         int count = 0;
-        double sum = 0.0;
 
-        System.out.println("Вас приветствует программа \"Побратски попилить счет\".\nНа скольких человек необходимо попилить ваш счёт...");
+        System.out.println("Вас приветствует программа \"Побратски попилить счет\".\n" +
+                "На скольких человек необходимо попилить ваш счёт, введите числовое значение >1 ...");
 
-
-        try {
-            numberOfPerson = scanner.nextInt();
-            checkingUserInput(numberOfPerson);
-
-        } catch (InputMismatchException e) {
-            System.out.println("Введите корректное значение используя цифры, а не буквы...");
-        }
+        numberOfPerson = checkingUserInputString();
+        checkingUserInputOfTaskCondition(numberOfPerson);
+        Calculate calculate = new Calculate();
 
         while (true) {
-            System.out.println("Вы хотите добавить товар? \n(Введите: \"да\" - если хотите добавить товар; \"завершить\" - если не хотите добавлять товар)...");
+            System.out.println("Вы хотите добавить позицию из счета? \n" +
+                    "(Введите: \"да\" - если хотите ее добавить; \"завершить\" - если не хотите добавлять)...");
             String userInput = scanner.next().toLowerCase(Locale.ROOT);
             switch (userInput) {
                 case "да": {
-                    System.out.println("Введите название товара...");
+                    System.out.println("Введите наименование позиции из счета...");
                     String nameProduct = scanner.next();
-                    System.out.println("Введите стоимость товара...");
-                    double costProduct = scanner.nextDouble();
+                    System.out.println("Введите стоимость позиции из счета...");
+                    double costProduct = checkingUserInputDouble();
+                    checkingValueProduct(costProduct);
                     Product product = new Product(nameProduct, costProduct);
-                    sum = sum + product.costProduct;
+                    calculate.addingProductsToCalculate(product);
+                    calculate.sum(costProduct);
                     break;
                 }
                 case "завершить": {
                     count++;
-                    System.out.println("Ввод продуктов завершен.");
+                    System.out.println("Ввод позиций по счету успешно завершен.\nДобавленные позиции:");
+                    calculate.addingProductsToConsole();
                     break;
                 }
                 default:
@@ -46,26 +45,57 @@ public class Main {
             }
         }
 
-        Calculate calculate = new Calculate(sum,numberOfPerson);
-        double division = calculate.division(sum, numberOfPerson);
-        if (sum == 0){
-            System.out.println("Товары отсутвуют, делить нет смысла. Завершение программы...");
-        }else System.out.println("Каждый человек должен скинуть - " + division);
+        double division = calculate.division(calculate.sum, numberOfPerson);
+        if (calculate.sum == 0){
+             System.out.println("Товары отсутвуют, пилить счет нет смысла. Завершение программы...");
+        }else System.out.println("\n" +
+                "Общая стоимость всех позиций счета = " + calculate.sum + "\n" +
+                "Количество человек, на которых необходимо попилить счет = " + numberOfPerson + "\n" +
+                "Каждый человек должен скинуться по - " + division);
 
     }
-    static void checkingUserInput (int number){
+    static int checkingUserInputString (){
+        Scanner scanner = new Scanner(System.in);
+        while (!scanner.hasNextInt()) {
+            System.out.println("Введите корректное значение, целое число больше 1-ого ...");
+            scanner.nextLine();
+        }return scanner.nextInt();
+    }
+
+    static int checkingUserInputOfTaskCondition(int number){
+        Scanner scanner = new Scanner(System.in);
         while (true) {
-            Scanner scanner = new Scanner(System.in);
             if (number == 1) {
-                System.out.println("Введите значение больше 1-ого...");
-                number = scanner.nextInt();
+                System.out.println("Введите значение >1...");
+                number = checkingUserInputString();
             } else if (number > 1) {
-                System.out.println("Введено корректное значение.");
+                System.out.println("Введено корректное значение количества человек!");
                 break;
-            } else {
-                System.out.println("Введите корректное значение, целое число больше 1-ого...");
-                number = scanner.nextInt();
+            } else if (number <= 0) {
+                System.out.println("Введено значение ноль или меньше нуля, повторите попытку...");
+                number = checkingUserInputString();
             }
-        }
+        }return number;
+    }
+
+    static double checkingUserInputDouble(){
+        Scanner scanner = new Scanner(System.in);
+        while(!scanner.hasNextDouble()) {
+            System.out.println("Необходимо ввести ЧИСЛОВОЕ значение стоимости позиции из счета, повторите попытку...");
+            scanner.nextLine();
+        }return scanner.nextDouble();
+
+    }
+
+    static double checkingValueProduct(double costProduct){
+        while (true) {
+            if (costProduct <= 0) {
+                System.out.println("Введено значение ноль или меньше нуля, повторите попытку...");
+                costProduct = checkingUserInputDouble();
+            }else if (costProduct > 0){
+                System.out.println( "Введено корректное значение стоимости позиции из счета!");
+                break;
+            }
+        }return costProduct;
     }
 }
